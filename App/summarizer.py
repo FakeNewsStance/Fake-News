35dea6bb732ba07206c9dff1eda660476c65a365
Girish <32607580+girishb98@users.noreply.github.com>
2019-02-10 11:55:30 +0530

    Update summarizer.py

--- App/summarizer.py
+++ App/summarizer.py
@@ -1,7 +1,17 @@
-from gensim.summarization import summarize
+"""from gensim.summarization import summarize
 
 class summarizer:
     def __init__(self, text = ""):
         self.text = text
     def summary(self):
-        return summarize(self.text)
+        return summarize(self.text)"""
+    
+from sumy.parsers.plaintext import PlaintextParser 
+from sumy.nlp.tokenizers import Tokenizer 
+from sumy.summarizers.lex_rank import LexRankSummarizer 
+
+file = "plain_text.txt" 
+parser = PlaintextParser.from_file(file, Tokenizer("english"))
+summarizer = LexRankSummarizer()
+
+summary = summarizer(parser.document, 5)
