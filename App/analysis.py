9ae042ef4796ce42a79306eb05248964e222382f
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2019-05-31 12:55:44 +0530

    Add analysis.py

---
+++ App/analysis.py
@@ -0,0 +1,17 @@
+import pymongo as pm
+
+client = pm.MongoClient("mongodb://localhost:27017/")
+db = client['fakenews']
+tweets = db['tweets']
+articles = db['articles']
+results = db['results']
+
+try:
+    with db.results.watch(
+            [{'$match': {'operationType': 'insert'}}]) as stream:
+        for insert_change in stream:
+            print(insert_change)
+except pymongo.errors.PyMongoError:
+    # The ChangeStream encountered an unrecoverable error or the
+    # resume attempt failed to recreate the cursor.
+    logging.error('...')
