66a8c2c65e32147069f0b757fbaa8d604dab2fd1
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2019-05-31 13:28:22 +0530

    Update analysis.py

--- App/analysis.py
+++ App/analysis.py
@@ -6,12 +6,14 @@ tweets = db['tweets']
 articles = db['articles']
 results = db['results']
 
-try:
-    with db.results.watch(
-            [{'$match': {'operationType': 'insert'}}]) as stream:
-        for insert_change in stream:
-            print(insert_change)
-except Exception as e:
-    # The ChangeStream encountered an unrecoverable error or the
-    # resume attempt failed to recreate the cursor.
-    print(e)
+total_tweets = tweets.count_documents({})
+total_tweets_with_articles = articles.count_documents({})
+total_tweets_with_score = results.count_documents({})
+score_max = results.find().sort({score:-1}).limit(1)
+score_min = results.find().sort({score:+1}).limit(1)
+
+print('\nTotal Tweets fetched :',total_tweets)
+print('Total tweets with articles :',total_tweets_with_articles)
+print('Total tweets with score:',total_tweets_with_score)
+print('Most credible tweet ',score_max)
+print('Least credible tweet ',score_min)
